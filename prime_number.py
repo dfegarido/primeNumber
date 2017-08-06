47e5186ff2814ae2bf2a2b4f5a96200b8146c752
Rorounifix <rorounifix@gmail.com>
2017-08-06 10:30:05 +0300

    update false return to exit the loop

--- prime_number.py
+++ prime_number.py
@@ -2,23 +2,26 @@ import time
 start_time = time.time()
 
 def prime(n):
+    '''Check if the Number is Prime or not'''
     zero = 0
     for i in range(1, n+1):
-        ans = n % i
-        if ans == 0:
+        if zero >= 2:
+            return False
+        if n % i == 0:
             zero += 1
-    return zero   
+    return zero
+        
 
-def list_of_prime(n):
+def list_of_prime(low, high):
+    '''Append all the prime number to a list'''
     x = []
-    for i in range(1, n+1):
+    for i in range(low, high+1):
         if prime(i) == 2:
             x.append(i)
     return x
 
-print(list_of_prime(100))
+print(list_of_prime(9000,10000))
 
-print('')
-print('')
-print('')
+
+
 print('It took = ',time.time() - start_time)
